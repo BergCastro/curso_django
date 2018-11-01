a0983c53d877c5857f95589274687ed4acb79e59
Lindemberg <fireberg2500@hotmail.com>
2018-11-01 12:04:30 -0300

    urls curso_djando modificed

--- curso_django/urls.py
+++ curso_django/urls.py
@@ -17,8 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from clientes import urls as clientes_urls
 from django.contrib.auth import views as auth_views
-from django.conf.urls.static import static
-from django.conf import settings
+
 from home import urls as home_urls
 
 urlpatterns = [
@@ -27,4 +26,4 @@ urlpatterns = [
     path('clientes/', include(clientes_urls)),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
