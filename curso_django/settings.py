722c531069f4f4fa1b4ca0a137fc4f8956977c98
Lindemberg <fireberg2500@hotmail.com>
2018-11-01 18:17:02 -0300

    settings altered statics

--- curso_django/settings.py
+++ curso_django/settings.py
@@ -51,9 +51,13 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+
 
 ]
+
+MIDDLEWARE_CLASSES = (
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+)
 
 ROOT_URLCONF = 'curso_django.urls'
 
