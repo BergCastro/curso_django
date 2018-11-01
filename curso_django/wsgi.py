eb93dae45586e8ae1ba23a7f9f9f005d748ceb2e
Lindemberg <fireberg2500@hotmail.com>
2018-11-01 12:09:52 -0300

    deleted whitenouse

--- curso_django/wsgi.py
+++ curso_django/wsgi.py
@@ -9,10 +9,9 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 
 import os
 
-from whitenoise.django import DjangoWhiteNoise
+from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gestao_clientes.settings")
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'curso_django.settings')
-
-
-application = DjangoWhiteNoise(application)
+application = Cling(get_wsgi_application())
